a0ea0e77a5c9276a4da0b4bbd8d950a6f63d1ae4
juanjohi <kennedywanjohi16@gmail.com>
2024-01-24 16:39:52 +0300

    version 1.0.4 loader commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import Skills from './components/Skills/Skills';
 import Works from './components/Works/Works';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
+import CustomLoader from './components/loader/CustomLoader';
+
+
 
 function App() {
+
   return (
     <div className="App">
+      <CustomLoader />
       <Navbar />
       <Intro />
       <Skills />
       <Works />
       <Contact />
-      <Footer/>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/loader/CustomLoader.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Loader from 'react-loaders'
+import './CustomLoader.scss'
+
+export const CustomLoader = () => {
+    return (
+      <>
+        <Loader type="pacman"/>
+      </>
+  )
+}
+
+export default CustomLoader
