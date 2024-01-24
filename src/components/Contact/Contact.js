d555cf4f89367720a5b46382e732c4f52ce8507e
juanjohi <kennedywanjohi16@gmail.com>
2024-01-24 18:06:18 +0300

    Contact form enabled

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -11,8 +11,8 @@ export const Contact = () => {
 
                 <div className="companiesList">
                     <p className="company"> JEMKOS <br /> MAZERAS</p>
-                    <p className="company"> MARINUS<br/> PLANTS</p>
-                    <p className="company"> NICS <br/> BOREHOLES</p>
+                    <p className="company"> MARINUS<br /> PLANTS</p>
+                    <p className="company"> NICS <br /> BOREHOLES</p>
                     <p className="company"> VINS <br />BOREHOLES </p>
                 </div>
 
@@ -22,11 +22,14 @@ export const Contact = () => {
                 <p className="contactPara">Fill Out the form below to discuss any work oppurtunities</p>
             </div>
 
-            <form action="" className="contactForm">
-                <input type="text" className="name" placeholder='Your Name' />
-                <input type="text" className="email" placeholder='Your Email'/>
-                <textarea type="text" rows="5" className='msg' placeholder='Your Message' />
-                <button className="submitBtn" type='submit' value='send'> Submit </button> 
+            <form action="https://formsubmit.co/kennedywanjohi15@gmail.com" method='POST' className="contactForm">
+                <input type="text" name="_honey" style={{ display: 'none' }} />
+                <input type="hidden" name="_captcha" value="false" />
+                <input type="hidden" name="_next" value="https://wanjohi-portfolio.vercel.app/portfolio/thanks.html"></input>
+                <input type="text" name='name' className="name" placeholder='Your Name' />
+                <input type="email" name='email' className="email" placeholder='Your Email' />
+                <textarea type="text" rows="5" className='msg' name='subject' placeholder='Your Message' />
+                <button className="submitBtn" type='submit' value='send'> Submit </button>
             </form>
 
         </section>
